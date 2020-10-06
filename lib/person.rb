b74b0c4ec7bf0c8559cb4881d46c333a3efef897
raaynaldo <raaynaldo@hotmail.com>
2020-10-06 15:59:33 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def inititalize(name, job)
+    @name = name
+    @job = job
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+end
